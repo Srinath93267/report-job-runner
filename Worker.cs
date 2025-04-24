d4cc41be904c40028b8f36272c009eac87863798
Srinath579 <62790179+Srinath579@users.noreply.github.com>
2025-04-24 11:28:07 +0530

    Sleep for 12 hours

--- Worker.cs
+++ Worker.cs
@@ -89,6 +89,8 @@ namespace ReportJobRunner
                     else
                     {
                         Console.WriteLine($"Connection String is Empty or Null \n Connection String: {connectionString}");
+                        Console.WriteLine("Going to Sleep for next 12 hours");
+                        await Task.Delay(60000 * 60 * 12 , stoppingToken);
                     }
                 }
             }
